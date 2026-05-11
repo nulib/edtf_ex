6eeffde5a74000407d907db530a0c3da458cc86d
weljoda <weljoda@gmail.com>
2026-05-11 11:24:11 +0200

    Fix: reject invalid single-digit date input not compliant with ISO 8601-2

--- test/edtf_test.exs
+++ test/edtf_test.exs
@@ -15,4 +15,17 @@ defmodule EDTFTest do
     assert EDTF.parse("bad date!") == {:error, :invalid_format}
     assert EDTF.parse("2020-%06-25?") == {:error, :invalid_format}
   end
+
+  test "rejects unprefixed short-digit years (ISO 8601-2 requires Y prefix or 4-digit form)" do
+    assert EDTF.parse("1") == {:error, :invalid_format}
+    assert EDTF.humanize("1") == {:error, :invalid_format}
+    assert EDTF.validate("1") == {:error, :invalid_format}
+
+    assert EDTF.parse("0001") ==
+             {:ok, %EDTF.Date{level: 0, type: :date, values: [1]}}
+
+    assert EDTF.parse("Y20020") ==
+             {:ok,
+              %EDTF.Date{level: 1, type: :year, values: [20_020], attributes: [significant: nil]}}
+  end
 end
